9874079e8e884b0e63753de381b153d31575f304
kasambuli <cynthiakasambuli@gmail.com>
2018-05-20 18:13:56 +0300

    add driver app urls

--- carpool/carpool/urls.py
+++ carpool/carpool/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^driver/', include('driver.urls')),
-    url(r'^driver/accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
     ]
