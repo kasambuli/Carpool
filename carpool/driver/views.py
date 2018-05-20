d3c925c669df04bed25d7078ca61f39b1b936932
kasambuli <cynthiakasambuli@gmail.com>
2018-05-20 18:22:47 +0300

    add index view

--- carpool/driver/views.py
+++ carpool/driver/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import DriverForm
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    if request.method == 'POST':
+        form = DriverForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = DriverForm()
+    return render(request, 'main/profile.html', {"form":form})
 
