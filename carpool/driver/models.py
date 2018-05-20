1bf9453e6940eb025cb7ceb23d78074990fc4ff8
kasambuli <cynthiakasambuli@gmail.com>
2018-05-20 18:14:28 +0300

    add driver model

--- carpool/driver/models.py
+++ carpool/driver/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Driver model.
+class Driver(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(blank = True,max_length = 30)
+    destination= models.TextField(max_length=100)
+    avatar = models.ImageField(upload_to = 'avatar/')
+    
+    def __str__(self):
+        return self.name
+
+    def save_driver(self):
+        self.save()
+        
+    @classmethod
+    def update_driver(cls,id,new_name):
+        cls.objects.filter(id=id).update(name = new_name)
+
+    @classmethod
+    def delete_driver(cls,id):
+        cls.objects.filter(id).delete()
+
